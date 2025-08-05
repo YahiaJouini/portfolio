80e52aaf53ef8511c944a9ea0a37c10e6d060720
YahiaJouini <jouiniyahya117@gmail.com>
2025-08-05 20:02:23 +0100

    feat: add Docker configuration and initial setup for Next.js application

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import { NextConfig } from "next"
 import createNextIntlPlugin from "next-intl/plugin"
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+   output: "standalone",
+   // compression will be configured using nginx
+   compress: false,
+}
 
 const withNextIntl = createNextIntlPlugin()
 export default withNextIntl(nextConfig)
